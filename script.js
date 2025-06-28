9e69544bea38a59650b612573cbda27126a1a0cb
gagoph <hectorgagop@outlook.com>
2025-06-27 20:36:23 -0400

    Integra singleplayer casi completo, combina trabajo de varias branches

--- script.js
+++ script.js
@@ -6,21 +6,59 @@ class Card {
         this.type = type;
         this.value = value;
     }
+
+    getDisplayText() {
+        if (this.type === "drawTwo") {
+            return "+2";
+        }
+        if (this.type === "reverse") {
+            return `
+                <svg width="32" height="32" viewBox="0 0 48 48" fill="none" style="vertical-align:middle;" xmlns="http://www.w3.org/2000/svg">
+                <rect width="48" height="48" fill="white" fill-opacity="0.01"/>
+                <path d="M34 6.67564C39.978 10.1337 44 16.5972 44 24M34 6.67564V14M34 6.67564H41.3244M41.3244 34C37.8663 39.978 31.4028 44 24 44M41.3244 34H34M41.3244 34V41.3244M14 41.3244C8.02199 37.8663 4 31.4028 4 24M14 41.3244V34M14 41.3244H6.67564M6.67564 14C10.1337 8.02199 16.5972 4 24 4M6.67564 14H14M6.67564 14V6.67564" stroke="#fff" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M41.3244 34C37.8663 39.978 31.4028 44 24 44M41.3244 34H34M41.3244 34V41.3244" stroke="#fff" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M14 41.3244C8.02199 37.8663 4 31.4028 4 24M14 41.3244V34M14 41.3244H6.67564" stroke="#fff" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M6.67566 14C10.1338 8.02199 16.5972 4 24 4M6.67566 14H14M6.67566 14V6.67564" stroke="#fff" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M34 6.67578C39.978 10.1339 44 16.5973 44 24.0001M34 6.67578V14.0001M34 6.67578H41.3244" stroke="#fff" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            `;
+        }
+        if (this.type === "jump" || this.type === "skip") {
+            return `
+                <svg width="32" height="32" viewBox="0 0 1920 1920" fill="none" style="vertical-align:middle;" xmlns="http://www.w3.org/2000/svg">
+                <path d="M213.333 960c0-167.36 56-321.707 149.44-446.4L1406.4 1557.227c-124.693 93.44-279.04 149.44-446.4 149.44-411.627 0-746.667-335.04-746.667-746.667m1493.334 0c0 167.36-56 321.707-149.44 446.4L513.6 362.773c124.693-93.44 279.04-149.44 446.4-149.44 411.627 0 746.667 335.04 746.667 746.667M960 0C429.76 0 0 429.76 0 960s429.76 960 960 960 960-429.76 960-960S1490.24 0 960 0" fill="#fff" fill-rule="evenodd"/>
+                </svg>
+            `;
+        }
+        return this.type === "number" ? this.value : this.type.toUpperCase();
+    }
+
+    getCssClasses() {
+        return `card ${this.color} ${this.type}`;
+    }
 }
 
 class Player {
-    constructor(id, name, cards = [], points, saidUNO, isHuman) {
+    constructor(id, name, cards = [], points = 0, saidUNO = false, isHuman = true) {
         this.id = id;
         this.name = name;
         this.cards = cards;
         this.points = points;
-        this.saidUNO = saidUNO; 
-        this.isHuman = isHuman; 
+        this.saidUNO = saidUNO;
+        this.isHuman = isHuman;
+    }
+
+    addCard(card) {
+        this.cards.push(card);
+    }
+
+    removeCard(index) {
+        return this.cards.splice(index, 1)[0];
     }
 }
 
 class Game {
-    constructor(players = [], deck = [], discardPile = [], turn, direction, currentColor, waitingForColor, roundWinner){
+    constructor(players = [], deck = [], discardPile = [], turn = 0, direction = 1, currentColor = null, waitingForColor = false, roundWinner = null) {
         this.players = players;
         this.deck = deck;
         this.discardPile = discardPile;
@@ -29,6 +67,14 @@ class Game {
         this.currentColor = currentColor;
         this.waitingForColor = waitingForColor;
         this.roundWinner = roundWinner;
+    }
+
+    getCurrentPlayer() {
+        return this.players[this.turn];
+    }
+
+    getTopDiscard() {
+        return this.discardPile[this.discardPile.length - 1];
     }
 }
 
@@ -40,20 +86,9 @@ const colors = ["red", "green", "blue", "yellow"];
 const specialCards = ["jump", "reverse", "drawTwo"];
 
 let players = [];
-let currentPlayerIndex = 0;
-let direction = 1; 
+let gameState = new Game(players, deck, discardPile);
 
-let gameState = new Game(players, deck, discardPile, currentPlayerIndex, direction, null, false, null);
-
-let buttonTest = document.getElementById("buttonTest");
-let container = document.getElementById("container");
-
-buttonTest.addEventListener("click", () => {
-    console.log("Button clicked!");
-    initializeDeck();
-    console.log(deck);
-    startGame(2); // Start game with 2 players for testing
-});
+// FUNCIONES ------------------------------------------------------------------------------------------------
 
 function initializeDeck() {
     deck = [];
@@ -65,135 +100,277 @@ function initializeDeck() {
             deck.push(new Card(color.charAt(0).toUpperCase() + "-" + type, color, type, 20));
         }
     }
-    shuffle();
+    shuffle(deck);
 }
 
-function shuffle() {
-    for (let cartaActual = deck.length - 1; cartaActual > 0; cartaActual--) {
-        const indiceRandom = Math.floor(Math.random() * (cartaActual + 1));
-        [deck[cartaActual], deck[indiceRandom]] = [deck[indiceRandom], deck[cartaActual]];
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
 }
 
-function startGame(numPlayers) {
-    for(let p = 0; p < numPlayers; p++) {
-        let playerName = prompt(`Enter name for Player ${p + 1}:`);
-        let isHuman = confirm(`Is Player ${p + 1} a human?`);
-        let player = new Player(p, playerName, [], 0, false, isHuman);
-        players.push(player);
-    }
-
-    gameState.players = players;
-    gameState.turn = 0;
-    gameState.direction = 1;
+function startGame(playerList) {
+    players = playerList;
+    gameState = new Game(players, deck, discardPile, 0, 1, null, false, null);
     dealCards();
 }
 
 function dealCards() {
-    for (let player of players) {
+    for (let player of gameState.players) {
+        player.cards = [];
         for (let i = 0; i < 7; i++) {
-            let card = deck.pop();
-            player.cards.push(card);
+            let card = gameState.deck.pop();
+            player.addCard(card);
         }
     }
-    discardPile.push(deck.pop());
-    gameState.discardPile = discardPile;
-    gameState.currentColor = discardPile[discardPile.length - 1].color;
-
-    console.log("Cards dealt to players:", players);
-    console.log("Top card on discard pile:", discardPile[discardPile.length - 1]);
-    console.log("Current color:", gameState.currentColor);
-    console.log("Game state after dealing cards:", gameState);
+    gameState.discardPile = [];
+    const firstCard = gameState.deck.pop();
+    gameState.discardPile.push(firstCard);
+    gameState.currentColor = firstCard.color;
 }
 
 function isValidPlay(card) {
-    let currentCard = discardPile[discardPile.length - 1];
-    if (card.color === gameState.currentColor || (card.type === currentCard.type && card.value === currentCard.value)) {
-        return true;
-    }
+    let currentCard = gameState.getTopDiscard();
+    return card.color === gameState.currentColor || (card.type === currentCard.type && card.value === currentCard.value);
 }
 
 function playCard(playerIndex, cardIndex) {
-    let player = players[playerIndex];
+    let player = gameState.players[playerIndex];
     let card = player.cards[cardIndex];
 
     if (isValidPlay(card)) {
-        player.cards.splice(cardIndex, 1);
-        discardPile.push(card);        
+        player.removeCard(cardIndex);
+        gameState.discardPile.push(card);
         gameState.currentColor = card.color;
-        console.log(`${player.name} jugó ${card.id}`);
-        
         nextTurn();
-    } else {
-        console.log("Invalid play.");
     }
 }
 
 function checkUNO(playerIndex) {
-    let player = players[playerIndex];
+    let player = gameState.players[playerIndex];
     if (player.cards.length === 1 && !player.saidUNO) {
-        player.saidUNO = true;  
-        console.log(`${player.name} said UNO!`);
+        player.saidUNO = true;
+        // Mostrar mensaje UNO
     } else if (player.cards.length === 0) {
-        console.log(`${player.name} has no cards left!`);
+        // Fin de partida
         endGame(player);
     }
 }
 
 function drawCard(playerIndex) {
-    let player = players[playerIndex];
+    let player = gameState.players[playerIndex];
     if (deck.length > 0) {
         let card = deck.pop();
-        player.cards.push(card);
-        console.log(`${player.name} drew a card: ${card.id}`);
-        
-    } else {
-        console.log("No cards left in the deck to draw.");
+        player.addCard(card);
     }
 }
 
 function nextTurn() {
     let nextPlayerIndex = gameState.turn + gameState.direction;
-
-    if (nextPlayerIndex < 0) {
-        nextPlayerIndex = players.length - 1;
-    }
-
-    if (nextPlayerIndex > players.length) {
-        nextPlayerIndex = 0;
-    }
-
+    if (nextPlayerIndex < 0) nextPlayerIndex = gameState.players.length - 1;
+    if (nextPlayerIndex >= gameState.players.length) nextPlayerIndex = 0;
     gameState.turn = nextPlayerIndex;
 }
 
-function countPoints(winnerIndex) {
-    let totalPoints = 0;
-    let winner = players[winnerIndex];
+function renderPlayerHand() {
+    const playerArea = document.getElementById("player-area");
+    playerArea.querySelector("#player-name").textContent = gameState.players[0].name;
 
-    for (let player of players) {
-        for (let card of player.cards) {
-            totalPoints += card.value;
+    // Renderiza la mano
+    let handDiv = playerArea.querySelector(".hand");
+    if (!handDiv) {
+        handDiv = document.createElement("div");
+        handDiv.className = "hand";
+        playerArea.appendChild(handDiv);
+    }
+    handDiv.innerHTML = "";
+
+    const player = gameState.players[0];
+    player.cards.forEach((card, idx) => {
+        const cardDiv = document.createElement("div");
+        cardDiv.className = card.getCssClasses();
+        cardDiv.innerHTML = card.getDisplayText();
+        cardDiv.onclick = () => handlePlayerPlay(idx);
+        handDiv.appendChild(cardDiv);
+    });
+}
+
+function renderOpponentHand() {
+    const opponentArea = document.getElementById("opponent-area");
+    opponentArea.innerHTML = "";
+    const cpu = gameState.players[1];
+    if (!cpu) return;
+    const handDiv = document.createElement("div");
+    handDiv.className = "hand";
+    for (let i = 0; i < cpu.cards.length; i++) {
+        const cardDiv = document.createElement("div");
+        cardDiv.className = "card back";
+        handDiv.appendChild(cardDiv);
+    }
+    opponentArea.appendChild(handDiv);
+}
+
+function renderCenterArea() {
+    // Mazo (deck)
+    const deckDiv = document.getElementById("deck");
+    deckDiv.innerHTML = ""; // Limpia el mazo
+
+    if (gameState.deck.length > 0) {
+        const deckCard = document.createElement("div");
+        deckCard.className = "card back";
+        deckCard.title = "Robar carta";
+        deckCard.onclick = () => {
+            if (gameState.getCurrentPlayer().isHuman) {
+                drawCard(0);
+                renderPlayerHand();
+                renderCenterArea();
+                renderOpponentHand();
+            }
+        };
+        deckDiv.appendChild(deckCard);
+
+        // Cantidad de cartas en el deck
+        const deckCount = document.createElement("span");
+        deckCount.id = "deck-count";
+        deckCount.className = "deck-count";
+        deckCount.textContent = gameState.deck.length;
+        deckDiv.appendChild(deckCount);
+    }
+
+    // Descarte (discard pile)
+    const discardDiv = document.getElementById("discard-pile");
+    discardDiv.innerHTML = "";
+    const topCard = gameState.getTopDiscard();
+    if (topCard) {
+        const discardCard = document.createElement("div");
+        discardCard.className = topCard.getCssClasses();
+        discardCard.innerHTML = topCard.getDisplayText();
+        discardDiv.appendChild(discardCard);
+    }
+}
+
+// Llama a renderPlayerHand() y renderCenterArea() después de cada jugada
+
+function handlePlayerPlay(cardIndex) {
+    const player = gameState.players[0];
+    const card = player.cards[cardIndex];
+    if (isValidPlay(card)) {
+        playCard(0, cardIndex);
+        renderPlayerHand();
+        renderCenterArea();
+        renderOpponentHand();
+        setTimeout(cpuTurn, 800);
+    } else {
+        alert("No puedes jugar esa carta.");
+    }
+}
+
+function cpuTurn() {
+    const cpu = gameState.players[1];
+    let played = false;
+    for (let i = 0; i < cpu.cards.length; i++) {
+        if (isValidPlay(cpu.cards[i])) {
+            playCard(1, i);
+            played = true;
+            break;
         }
     }
-
-    winner.points += totalPoints;
-    console.log(`${winner.name} has ${winner.points} points.`);
-}
-
-function resetRound() {
-    gameState.discardPile = [];
-    gameState.deck = [];
-    gameState.turn = 0;
-    gameState.direction = 1;
-    gameState.currentColor = null;
-    gameState.waitingForColor = false;
-    gameState.roundWinner = null;
-
-    for (let player of players) {
-        player.cards = [];
-        player.saidUNO = false;
+    if (!played) {
+        drawCard(1);
     }
-
-    initializeDeck();
-    dealCards();
+    renderPlayerHand();
+    renderCenterArea();
+    renderOpponentHand();
 }
+
+// EVENTOS --------------------------------------------------------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", function() {
+    const singleBtn = document.getElementById("singleplayer-btn");
+    const multiBtn = document.getElementById("multiplayer-btn");
+    const multiOptions = document.getElementById("multiplayer-options");
+
+    singleBtn.addEventListener("click", () => {
+        document.getElementById("welcome-screen").classList.add("hidden");
+        document.getElementById("game-board").classList.remove("hidden");
+        initializeDeck();
+        const player1 = new Player(0, "Tú", [], 0, false, true);
+        const player2 = new Player(1, "CPU", [], 0, false, false);
+        startGame([player1, player2]);
+        renderPlayerHand();
+        renderCenterArea();
+        renderOpponentHand(); 
+    });
+
+    multiBtn.addEventListener("click", () => {
+        multiOptions.classList.remove("hidden");
+    });
+
+    const startMultiplayerBtn = document.getElementById("start-multiplayer-btn");
+    const playerSetupModal = document.getElementById("player-setup-modal");
+    const playerInputsDiv = document.getElementById("player-inputs");
+    const playerSetupForm = document.getElementById("player-setup-form");
+    const cancelSetupBtn = document.getElementById("cancel-setup");
+
+    startMultiplayerBtn.addEventListener("click", () => {
+        const numPlayers = parseInt(document.getElementById("player-count").value, 10);
+        playerInputsDiv.innerHTML = "";
+        for (let i = 0; i < numPlayers; i++) {
+            const playerDiv = document.createElement("div");
+            playerDiv.className = "player-input-row";
+            playerDiv.innerHTML = `
+                <label class="player-label">Jugador ${i + 1}:
+                    <input type="text" class="player-name-input" name="playerName${i}" placeholder="Nombre" required>
+                </label>
+                <label class="toggle-label">
+                    <input type="checkbox" class="toggle-human" name="isHuman${i}" checked data-index="${i}">
+                    <span class="toggle-status" id="toggle-status-${i}">Humano</span>
+                </label>
+            `;
+            playerInputsDiv.appendChild(playerDiv);
+        }
+        playerSetupModal.classList.remove("hidden");
+
+        const toggles = playerInputsDiv.querySelectorAll('.toggle-human');
+        toggles.forEach(toggle => {
+            toggle.addEventListener('change', function() {
+                const idx = this.getAttribute('data-index');
+                const statusSpan = document.getElementById(`toggle-status-${idx}`);
+                statusSpan.textContent = this.checked ? 'Humano' : 'CPU';
+            });
+        });
+    });
+
+    cancelSetupBtn.addEventListener("click", () => {
+        playerSetupModal.classList.add("hidden");
+    });
+
+    playerSetupForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const numPlayers = playerInputsDiv.children.length;
+        const playerList = [];
+        for (let i = 0; i < numPlayers; i++) {
+            const name = playerSetupForm[`playerName${i}`].value || `Jugador ${i+1}`;
+            const isHuman = playerSetupForm[`isHuman${i}`].checked;
+            playerList.push(new Player(i, name, [], 0, false, isHuman));
+        }
+        playerSetupModal.classList.add("hidden");
+        document.getElementById("welcome-screen").classList.add("hidden");
+        document.getElementById("game-board").classList.remove("hidden");
+        initializeDeck();
+        startGame(playerList);
+        renderPlayerHand();
+        renderCenterArea();
+    });
+
+    document.getElementById("uno-btn").addEventListener("click", () => {
+        const player = gameState.players[0];
+        if (player.cards.length === 1 && !player.saidUNO) {
+            player.saidUNO = true;
+            alert("¡UNO!");
+        } else {
+            alert("Solo puedes decir UNO cuando te queda una carta.");
+        }
+    });
+});
